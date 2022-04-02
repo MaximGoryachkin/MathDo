c964613b854f7c0f3028fef3c966401351f59100
Maxim Goryachkin <maxgoryachkin@mail.ru>
2022-04-02 14:08:15 +0300

    Adding SyntaxManualViewController settings

--- MathDo/FormulaCreating/SyntaxManual/SyntaxManualViewController.swift
+++ MathDo/FormulaCreating/SyntaxManual/SyntaxManualViewController.swift
@@ -6,14 +6,40 @@
 //
 
 import UIKit
+import WebKit
 
 class SyntaxManualViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    
+    private lazy var webView: WKWebView = {
+        let view = WKWebView()
+        view.backgroundColor = .systemBlue
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
         view.backgroundColor = .white
-        // Do any additional setup after loading the view.
+        view.addSubview(webView)
+        setWebView()
+        
+        let htmlPath = Bundle.main.path(forResource: "FormulaTable", ofType: "html")
+        guard let htmlPath = htmlPath else { return }
+        let url = URL(fileURLWithPath: htmlPath)
+        let request = URLRequest(url: url)
+        webView.load(request)
+        
     }
     
-
+    private func setWebView() {
+        var constraints = [NSLayoutConstraint]()
+        constraints.append(webView.leadingAnchor.constraint(greaterThanOrEqualTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 10))
+        constraints.append(webView.trailingAnchor.constraint(greaterThanOrEqualTo: view.safeAreaLayoutGuide.trailingAnchor))
+        constraints.append(webView.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor))
+        constraints.append(webView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 10))
+        constraints.append(webView.heightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.heightAnchor))
+        
+        NSLayoutConstraint.activate(constraints)
+    }
+    
 }
